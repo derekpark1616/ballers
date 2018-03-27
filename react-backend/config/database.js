c3479dec4bdfb8e83675777d53b44434563a86e5
PARK <dp619h@att.com>
2018-03-27 15:22:29 -0700

    mongodb now hosted by mLab

--- react-backend/config/database.js
+++ react-backend/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/vballers',
+    //database: 'mongodb://localhost:27017/vballers',
+    database: 'mongodb://dongwonderful16:meandyou16@ds125469.mlab.com:25469/heroku_d3t9hnmk'
 }
